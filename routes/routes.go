fc160ed30eced0e105b343edb4c7bbd77afab3a9
Cahil <cahil712@gmail.com>
2024-03-07 23:12:26 +0200

    feat: Implemented test handlers

--- routes/routes.go
+++ routes/routes.go
@@ -1,11 +1,32 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"next-month/service/handler"
 
-func Get(r *gin.Engine) {
+	"github.com/gin-gonic/gin"
+)
+
+func Get(r *gin.Engine, db *sql.DB) {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+		})
+	})
+
+	r.GET("/test", func(c *gin.Context) {
+		handler.CreateTest(db)
+
+		c.JSON(200, gin.H{
+			"message": "test",
+		})
+	})
+
+	r.GET("/test-drop", func(c *gin.Context) {
+		handler.ClearTest(db)
+
+		c.JSON(200, gin.H{
+			"message": "test",
 		})
 	})
 }
